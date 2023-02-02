d4c3ec87a0657fb2281362ef595b0e1ff6f37c7d
Alice <alicemoraaongongo@gmail.com>
2023-02-02 09:27:44 +0300

    Students records

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var name: String = "Alice Moraa"
+    println (name)
+    var age = 23
+    println(age)
+    var amKenyan: Boolean = true
+    var weight = 56
+    println(weight)
+    var phoneNumber = "0759547300"
+    println(phoneNumber)
+
+
+
+
+}
